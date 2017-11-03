e4af5eb5f77babe3cfb16b6069790c17669e1853
d-jeph <jephdavid@gmail.com>
2017-11-03 23:38:05 +0300

    add django authentication

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     # post views
-    url(r'^login/$', views.user_login, name='login'),
+    #url(r'^login/$', views.user_login, name='login'),
+    url(r'^$', views.dashboard, name='dashboard'),
+    # login / logout urls
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^logout-then-login/$', auth_views.logout_then_login, name='logout_then_login'),
+
+
     ]
