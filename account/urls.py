7d24cf34ae18f013ac4558a136edaeade780465a
d-jeph <jephdavid@gmail.com>
2017-11-10 20:58:17 +0300

    Add follow and unfollow feature

--- account/urls.py
+++ account/urls.py
@@ -25,5 +25,10 @@ urlpatterns = [
     url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^password-reset/complete/$',auth_views.password_reset_complete, name='password_reset_complete'),
 
+     # user profiles
+    url(r'^users/$', views.user_list, name='user_list'),
+    url(r'^users/follow/$', views.user_follow, name='user_follow'),
+    url(r'^users/(?P<username>[-\w]+)/$', views.user_detail, name='user_detail'),
+
 
     ]
--- account/views.py
+++ account/views.py
@@ -1,10 +1,16 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm,UserRegistrationForm,UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import Profile
+from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
+from django.contrib.auth.models import User
+#from actions.models import Action
+#from actions.utils import create_action
+
+from .models import Profile,Contact
+from .forms import LoginForm,UserRegistrationForm,UserEditForm, ProfileEditForm
 
 # Create your views here.
 def user_login(request):
@@ -70,3 +76,36 @@ def edit(request):
 @login_required
 def dashboard(request):
     return render(request,'account/dashboard.html',{'section': 'dashboard'})
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(request, 'account/user/list.html', {'section': 'people',
+                                                      'users': users})
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User, username=username, is_active=True)
+    return render(request, 'account/user/detail.html', {'section': 'people',
+                                                        'user': user})
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(user_from=request.user,
+                                              user_to=user)
+                #create_action(request.user, 'is following', user)
+            else:
+                Contact.objects.filter(user_from=request.user,
+                                       user_to=user).delete()
+            return JsonResponse({'status':'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status':'ko'})
+    return JsonResponse({'status':'ko'})
